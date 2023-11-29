71c8325d07cde26aa5be9bc7cfbd6aaf55e5797d
SarahWeissfish <sariwaisfish@gmail.com>
2023-11-30 01:31:18 +0200

    dependencies injection

---
+++ school1/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using school.Entities;
+using school1.Entities;
+
+namespace school1
+{
+    public class DataContext
+    {
+
+        public List<Course> courses;
+        public List<Student> students;
+        public List<Teacher> teachers;
+        public DataContext() { 
+            courses = new List<Course>();   
+            students = new List<Student>(); 
+            teachers= new List<Teacher>();
+        
+        
+        }
+    }
+}
